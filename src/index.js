5bbac8754e85f455e27391d72220f6e6dca15c09
nmistry45 <nmistry45@gmail.com>
2020-06-03 18:04:30 +0530

    redux added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
   <React.StrictMode>
-  	<App />
+  	<Provider store={store}>
+  		<App />
+  	</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
