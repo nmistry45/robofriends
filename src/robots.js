a966b65fda3dbf22a8e79b543844f3f21a8c152f
nmistry45 <nmistry45@gmail.com>
2020-06-03 14:13:54 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Nishit Mistry',
+    username: 'nishitmistry24',
+    email: 'nmistry45@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Neha Kirtikar',
+    username: 'nehaoverhere',
+    email: 'nehakirtikar15@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Maitreya Bapat',
+    username: 'maitreya2205',
+    email: 'maitreya2205@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Parth Joshi',
+    username: 'parthj',
+    email: 'parth010872@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Kewal Doshi',
+    username: 'k_wal10',
+    email: 'kewaldoshi7@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Khyati Prajapati',
+    username: 'khyatip19',
+    email: 'khyatip19@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Mansi Thakkar',
+    username: 'mansi__thakkar',
+    email: 'mansi.thakkar@sakec.ac.in'
+  },
+  {
+    id: 8,
+    name: 'Charmi Sanghavi',
+    username: 'iam_4mi',
+    email: 'charmi.sanghavi@sakec.ac.in'
+  },
+  {
+    id: 9,
+    name: 'Janak Bhadresha',
+    username: 'jb_janak',
+    email: 'janakbhadresha@gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Virti Parekh',
+    username: 'virti_parekh',
+    email: 'virtiparekh03@gmail.com'
+  }
+];
